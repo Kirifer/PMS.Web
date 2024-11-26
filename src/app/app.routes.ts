cac6a4d613ee6524691c14282a2eff34b6d8425a
Gian Cabigting <gdcabigting.dev@gmail.com>
2024-11-26 10:19:26 +0800

    remove: unnecessary files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,32 +2,23 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { PerfReviewComponent } from './pages/perf-review/perf-review.component';
 import { UsersComponent } from './pages/users/users.component';
-import { PerfReviewComponent2 } from './pages/perf-review-copy/perf-review.component';
 import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-    {
-        path: 'dashboard', 
-        component: DashboardComponent
-    
-    },
-    {
-        path: 'perf-rev', 
-        component: PerfReviewComponent
-    
-    },
-    {
-        path: 'users', 
-        component: UsersComponent
-    
-    },
-    {
-        path: 'perf-rev-2',
-        component: PerfReviewComponent2
-    },
-    {
-        path: 'login',
-        component: LoginComponent
-    }
-
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'perf-rev',
+    component: PerfReviewComponent,
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
 ];
