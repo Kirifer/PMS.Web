6f11a1a3ceb82190b3c7ed748742d374e938a2be
Gian Cabigting <gdcabigting.dev@gmail.com>
2024-11-28 16:05:12 +0800

    ref: change name input to dropdown

--- src/app/pages/perf-review/components/add-record/constants/data.constants.ts
+++ src/app/pages/perf-review/components/add-record/constants/data.constants.ts
@@ -1,99 +1,44 @@
 export const EMPLOYEE_INITIAL_STATE = {
-    startDate: '',
-    endDate: '',
-  };
-  
-  export const EMPLOYEE_DATA_INITIAL_STATE = {
-    name: '',
-    departmentType: '',
-    startYear: '',
-    endYear: '',
-    supervisorId: '',
-    startDate: '',
-    endDate: '',
-    activeSupervisor: false,
-  };
-  
-  export const COMPETENCY_DATA_INITIAL_STATE = [
-    {
-      competencyId: '',
-      orderNo: 1,
-      weight: 0,
-    },
-    {
-      competencyId: '',
-      orderNo: 2,
-      weight: 0,
-    },
-    {
-      competencyId: '',
-      orderNo: 3,
-      weight: 0,
-    },
-    {
-      competencyId: '',
-      orderNo: 4,
-      weight: 0,
-    },
-  ];
-  
-  export const GOALS_DATA_INITIAL_STATE = [
-    {
-      orderNo: 1,
-      goals: '',
-      weight: 0,
-      date: '',
-      measure4: '',
-      measure3: '',
-      measure2: '',
-      measure1: '',
-    },
-    {
-      orderNo: 2,
-      goals: '',
-      weight: 0,
-      date: '',
-      measure4: '',
-      measure3: '',
-      measure2: '',
-      measure1: '',
-    },
-    {
-      orderNo: 3,
-      goals: '',
-      weight: 0,
-      date: '',
-      measure4: '',
-      measure3: '',
-      measure2: '',
-      measure1: '',
-    },
-    {
-      orderNo: 4,
-      goals: '',
-      weight: 0,
-      date: '',
-      measure4: '',
-      measure3: '',
-      measure2: '',
-      measure1: '',
-    },
-    {
-      orderNo: 5,
-      goals: '',
-      weight: 0,
-      date: '',
-      measure4: '',
-      measure3: '',
-      measure2: '',
-      measure1: '',
-    },
-  ];
-  
-  export const TABS = [
-    { label: 'Employee Details' },
-    { label: 'Goals' },
-    { label: 'Competencies' },
-    { label: 'Confirmation' },
-  ];
-  
+  startDate: '',
+  endDate: '',
+};
+
+export const EMPLOYEE_DATA_INITIAL_STATE = {
+  name: '',
+  departmentType: '',
+  startYear: '',
+  endYear: '',
+  supervisorId: '',
+  startDate: '',
+  endDate: '',
+  activeSupervisor: false,
+};
+
+export const COMPETENCY_DATA_INITIAL_STATE = Array(4)
+  .fill({
+    competencyId: '',
+    weight: 0,
+  })
+  .map((item, index) => ({ ...item, orderNo: index + 1 }));
+
+export const GOALS_DATA_INITIAL_STATE = Array(5)
+  .fill({
+    orderNo: 0,
+    goals: '',
+    weight: 0,
+    date: '',
+    measure4: '',
+    measure3: '',
+    measure2: '',
+    measure1: '',
+  })
+  .map((item, index) => ({ ...item, orderNo: index + 1 }));
+
+export const LOOKUP_USERS = [];
+
+export const TABS = [
+  { label: 'Employee Details' },
+  { label: 'Goals' },
+  { label: 'Competencies' },
+  { label: 'Confirmation' },
+];
