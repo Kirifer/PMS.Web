351e011c1917affa300ae2033a9d1edcc5ee099e
Gian Cabigting <gdcabigting.dev@gmail.com>
2024-12-03 11:09:59 +0800

    style: table redesign

--- src/app/module/admin/perf-review/perf-review.component.spec.ts
+++ src/app/module/admin/perf-review/perf-review.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PerfReviewComponent } from './perf-review.component';
+import { PerformanceReviewComponent } from './perf-review.component';
 
 describe('PerfReviewComponent', () => {
-  let component: PerfReviewComponent;
-  let fixture: ComponentFixture<PerfReviewComponent>;
+  let component: PerformanceReviewComponent;
+  let fixture: ComponentFixture<PerformanceReviewComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PerfReviewComponent]
+      imports: [PerformanceReviewComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(PerfReviewComponent);
+    fixture = TestBed.createComponent(PerformanceReviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
